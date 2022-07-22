78018f9c475de386dc07e7b3428cc5b5aedc0aca
Mritunjay Upadhyay <mupadhyay00@gmail.com>
2022-07-22 22:12:41 +0700

    feat: Add router

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './home.scss';
+
+function Home() {
+    return (
+        <div className="HomePage">
+            <p>Home</p>
+        </div>
+    );
+}
+
+export { Home };
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './login.scss';
+
+function Login() {
+    return (
+        <div className="HomePage">
+            <p>Login</p>
+        </div>
+    );
+}
+
+export { Login };
